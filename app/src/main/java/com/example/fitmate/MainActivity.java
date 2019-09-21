656b6d09a23eb9bcaa6310e4582edc6f496daaf6
abhinavsaxena99 <abhinav.saxena10@gmail.com>
2019-09-21 16:15:17 +0530

    DIetssss

--- app/src/main/java/com/example/fitmate/MainActivity.java
+++ app/src/main/java/com/example/fitmate/MainActivity.java
@@ -31,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
