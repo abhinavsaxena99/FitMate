e3d578c2051cde36c46dcfd9992a7e01da1114b7
mittal-ayush <ayushmittal0297@gmail.com>
2019-09-22 05:53:19 +0530

    Everything ready.

--- app/src/main/java/com/example/fitmate/Exercise.java
+++ app/src/main/java/com/example/fitmate/Exercise.java
@@ -2,25 +2,19 @@ package com.example.fitmate;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.method.LinkMovementMethod; 
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.fitmate.ui.main.SectionsPagerAdapter;
+import android.widget.TextView;
 
 public class Exercise extends Fragment {
+
+//    ListView listView;
 
     @Nullable
     @Override
@@ -28,6 +22,28 @@ public class Exercise extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.exercise, container, false);
+
+        TextView text1=(TextView)rootView.findViewById(R.id.textView1);
+        TextView text2=(TextView)rootView.findViewById(R.id.textView2);
+        TextView text3=(TextView)rootView.findViewById(R.id.textView3);
+        TextView text4=(TextView)rootView.findViewById(R.id.textView4);
+        TextView text5=(TextView)rootView.findViewById(R.id.textView5);
+        TextView text6=(TextView)rootView.findViewById(R.id.textView6);
+        text1.setMovementMethod(LinkMovementMethod.getInstance());
+        text2.setMovementMethod(LinkMovementMethod.getInstance());
+        text3.setMovementMethod(LinkMovementMethod.getInstance());
+        text4.setMovementMethod(LinkMovementMethod.getInstance());
+        text5.setMovementMethod(LinkMovementMethod.getInstance());
+        text6.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+//        listView=rootView.findViewById(R.id.listView);
+
+
+
+
+
         return rootView;
     }
 }
